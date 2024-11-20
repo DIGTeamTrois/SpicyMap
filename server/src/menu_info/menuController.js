4d26605648d6d3397f9c7b923eccdfdc2fa5e0d1
t-kunita <138644278+t-kunita@users.noreply.github.com>
2024-11-21 07:32:15 +0900

    Update menuController.js

--- server/src/menu_info/menuController.js
+++ server/src/menu_info/menuController.js
@@ -2,12 +2,16 @@ const menuModel = require("./menuModel")
 
 module.exports = {
     async all(req, res){
-        const menuData = await menuModel.all()
-        res.status(200).send(menuData)
-        // if(menuData.length !== 0){
-        //     res.status(200).send(menuData)
-        // }else{
-        //     res.status(400).json("Could not get menu's data.")
-        // }
+        try{
+                const menuData = await menuModel.all()
+                if(menuData){
+                    res.status(200).json(menuData)
+                }else{
+                    res.status(400).json("Could not get menu's data.")
+                }
+        } catch (error) {
+                console.log("Internal Server Error(menu data)", error)
+                res.status(500).json({error: "Internal Server Error"});
+        }
     }
 }
