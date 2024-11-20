58309a97549d7f11ac7c975910b34b9a1f07730b
QaMuu <44154881+QaMuu@users.noreply.github.com>
2024-11-20 13:58:12 +0900

    初回デプロイ用コミット

---
+++ server/src/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+function setupServer() {
+  const app = express();
+  app.use(cors());
+
+  app.use(express.json());
+  app.use("/", express.static("../frontend/dist/"));
+
+  return app;
+}
+
+module.exports = {
+  setupServer,
+};
