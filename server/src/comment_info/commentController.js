e970a023ab957f2a6fbd2eeb85f4dd67361f2cec
t-kunita <138644278+t-kunita@users.noreply.github.com>
2024-11-21 07:31:33 +0900

    Update commentController.js

--- server/src/comment_info/commentController.js
+++ server/src/comment_info/commentController.js
@@ -2,12 +2,16 @@ const commentModel=require("./commentModel")
 
 module.exports= {
     async all(req, res) {
-        const commentData = await commentModel.all();
-        res.status(200).send(commentData)
-        // if (commentData.length !== 0) {
-        //     res.status(200).send(commentData)
-        // } else {
-        //     res.status(400).json("Could not get comment's data.")
-        // }
+        try{
+            const commentData = await commentModel.all();
+            if (commentData) {
+                res.status(200).json(commentData)
+            } else {
+                res.status(400).json("Could not get comment's data.")
+            }
+        } catch (error) {
+            console.log("Internal Server Error(comment data)", error)
+            res.status(500).json({error: "Internal Server Error"});
+        }
     }
 }
