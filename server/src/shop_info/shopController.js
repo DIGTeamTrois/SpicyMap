bbe41b8c27d3908d80d046d586c0b8278e98fd20
t-kunita <138644278+t-kunita@users.noreply.github.com>
2024-11-21 07:32:45 +0900

    Update shopController.js

--- server/src/shop_info/shopController.js
+++ server/src/shop_info/shopController.js
@@ -1,13 +1,17 @@
 const shopModel = require("./shopModel")
 
 module.exports = {
-    async all(req, res){
-        const shopData = await shopModel.all()
-        res.status(200).send(shopData)
-        // if(!shopData){
-        //     res.status(200).send(shopData)
-        // }else{
-        //     res.status(400).json("Could not get shop's data.")
-        // }
+    async all(req, res) {
+        try {
+            const shopData = await shopModel.all()
+            if (shopData) {
+                res.status(200).json(shopData)
+            }else {
+            res.status(400).json("Could not get shop's data.")
+            }
+        } catch (error) {
+            console.log("Internal Server Error(shop data)", error)
+            res.status(500).json({error: "Internal Server Error"});
+        }
     }
 }
