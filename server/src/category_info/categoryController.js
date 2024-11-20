458f1ee6b2c809e8fabde48dcdff9710f7d599f6
t-kunita <138644278+t-kunita@users.noreply.github.com>
2024-11-21 07:30:33 +0900

    Update categoryController.js

--- server/src/category_info/categoryController.js
+++ server/src/category_info/categoryController.js
@@ -2,12 +2,16 @@ const categoryModel=require("./categoryModel")
 
 module.exports= {
     async all(req, res) {
-        const categoryData = await commentModel.all();
-        res.status(200).send(categoryData)
-        // if (categoryData.length !== 0) {
-        //     res.status(200).send(categoryData)
-        // } else {
-        //     res.status(400).json("Could not get category's data.")
-        // }
+        try {
+            const categoryData = await commentModel.all();
+            if (categoryData) {
+                res.status(200).json(categoryData)
+            } else {
+                res.status(400).json("Could not get category's data.")
+            }
+        } catch (error) {
+            console.log("Internal Server Error(category data)", error)
+            res.status(500).json({error: "Internal Server Error"});
+        }
     }
 }
