39d0b37d339c44dabd9dc40650816d7d78802343
natsuki_suzuki <natsuki_suzuki_aa@mail.toyota.co.jp>
2024-11-21 11:33:12 +0900

    shop表示

--- frontend/src/components/IndexView.tsx
+++ frontend/src/components/IndexView.tsx
@@ -2,60 +2,33 @@ import {Status, Wrapper} from "@googlemaps/react-wrapper";
 
 import {Map} from "./Map";
 import {Marker} from "./Marker.tsx";
-import {useEffect} from "react";
-import {useAtom, useSetAtom} from "jotai";
+import {useAtom} from "jotai";
 
-import {shopsAtom, menusAtom, commentsAtom, categoriesAtom} from "./atom.tsx";
+import {shopsAtom} from "./atom.tsx";
 
 export default function IndexView() {
+
+  const [shops] = useAtom(shopsAtom)
+
   const position = {
-    lat: 35.17179780732516,
-    lng: 136.88172163100913
+    lat: 35.17100383703858,
+    lng: 136.88064007022157
   } as google.maps.LatLngLiteral;
 
-  const [shops, setShops] = useAtom(shopsAtom)
-  const setMenus = useSetAtom(menusAtom)
-  const setComments = useSetAtom(commentsAtom)
-  const setCategories = useSetAtom(categoriesAtom)
+  const markers:google.maps.LatLngLiteral[] = [{lat:shops[0].latitude,lng:shops[0].longitude}];
 
-
-  useEffect(() => {
-
-    fetch("/shops")
-        .then(res => res.json())
-        .then(data => setShops(data))
-
-    fetch("/menus")
-        .then(res => res.json())
-        .then(data => setMenus(data))
-
-    fetch("/comments")
-        .then(res => res.json())
-        .then(data => setComments(data))
-
-    fetch("/categories")
-        .then(res => res.json())
-        .then(data => setCategories(data))
-
-  }, []);
-
-  const markers:google.maps.LatLngLiteral[] = shops.map((value: object) => {
-    return {lat: value.latitude, lng: value.longitude}
-  })
-
-
-    return (
-        <div className='flex flex-row w-1/2 justify-center'>
-          <Wrapper apiKey='AIzaSyAvSUDOsyJyk9Hotfx0PMVdyKnzSgngHKQ' render={render}>
-            <Map style={{ width: '100%', aspectRatio: '16 / 9' }} center={position}>
-              {markers.map((value) => {
-                return <Marker position={value} />
-              })}
-            </Map>
-          </Wrapper>
-        </div>
-    )
-  }
+  return (
+      <div className='flex flex-row w-1/2 justify-center'>
+        <Wrapper apiKey='AIzaSyAvSUDOsyJyk9Hotfx0PMVdyKnzSgngHKQ' render={render}>
+          <Map style={{ width: '100%', aspectRatio: '16 / 9' }} center={position}>
+            {markers.map((value, index) => {
+              return <Marker key={index} position={value} />
+            })}
+          </Map>
+        </Wrapper>
+      </div>
+  )
+}
 
 const render = (status: Status) => {
   return <h1>{status}</h1>
--- frontend/src/components/atom.tsx
+++ frontend/src/components/atom.tsx
@@ -1,6 +1,22 @@
 import  { atom } from 'jotai'
 
-export const shopsAtom = atom<object[]>([])
-export const menusAtom = atom<object[]>([])
-export const commentsAtom = atom<object[]>([])
-export const categoriesAtom = atom<object[]>([])
+interface shopInterface {
+    id: number,
+    shop: string,
+    average_spicy: number,
+    category_id: number,
+    latitude: number,
+    longitude: number,
+}
+
+export const shopsAtom = atom<shopInterface[]>([{
+    id: 1,
+    shop: "韓国食堂",
+    average_spicy: 3.2,
+    category_id: 1,
+    latitude: 35.17,
+    longitude: 136.86,
+}])
+// export const menusAtom = atom<object[]>([])
+// export const commentsAtom = atom<object[]>([])
+// export const categoriesAtom = atom<object[]>([])
