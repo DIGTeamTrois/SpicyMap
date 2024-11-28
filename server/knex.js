74af6bf53e99f870a128dd3906fc5f67191c53de
Tora <toranosuke_kakizono@mail.toyota.co.jp>
2024-11-28 10:01:43 +0900

    knex変更

--- server/knex.js
+++ server/knex.js
@@ -1,7 +1,10 @@
 require("dotenv").config({ path: "./.env" });
-const environment = process.env.RENDER
-  ? "production"
-  : process.env.NODE_ENV || "development";
-const config = require("./knexfile.js")[environment];
-const knex = require("knex")(config);
-module.exports = knex;
+// const environment = process.env.RENDER_ENV;
+// const config = require("./knexfile.js")[environment];
+// const knex = require("knex")(config);
+// module.exports = knex;
+
+const knex = require("knex");
+const knexConfig = require("./knexfile.js");
+
+module.exports = knex(knexConfig[process.env.NODE_ENV]);
