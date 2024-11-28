b4fdb2de99d2b1f7c416dcdbad7526a68235776f
Tora <toranosuke_kakizono@mail.toyota.co.jp>
2024-11-28 09:46:23 +0900

    環境変数指定コードの変更

--- server/knex.js
+++ server/knex.js
@@ -1,5 +1,7 @@
 require("dotenv").config({ path: "./.env" });
-const environment = process.env.NODE_ENV
+const environment = process.env.RENDER
+  ? "production"
+  : process.env.NODE_ENV || "development";
 const config = require("./knexfile.js")[environment];
 const knex = require("knex")(config);
 module.exports = knex;
