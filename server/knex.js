95e9f326685e0b3043d599e9b81a73e1867703c9
Tora <toranosuke_kakizono@mail.toyota.co.jp>
2024-11-28 10:07:44 +0900

    不要なコメントを削除

--- server/knex.js
+++ server/knex.js
@@ -1,8 +1,4 @@
 require("dotenv").config({ path: "./.env" });
-// const environment = process.env.RENDER_ENV;
-// const config = require("./knexfile.js")[environment];
-// const knex = require("knex")(config);
-// module.exports = knex;
 
 const knex = require("knex");
 const knexConfig = require("./knexfile.js");
